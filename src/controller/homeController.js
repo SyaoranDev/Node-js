d3daac602db7cd8d344b953e783e11c9605d4957
Anh Tu Dev <daoanhtu1991@gmail.com>
2023-04-16 22:58:19 +0700

    HTTP REQUEST METHOD & Database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,8 +1,21 @@
-let getHomepage = (req,res) => {
-    //logic
-    return res.render('test/index.ejs');
-}
+import connection from "../config/connectDB";
+
+let getHomepage = (req, res) => {
+  //logic
+    let data = [];
+    connection.query("SELECT * FROM `users`", 
+    function (err, results, fields) {
+        console.log("check my sql");
+        console.log(results); // results contains rows returned by server
+        data = results.map((row) => {
+        return row;
+    });
+    return res.render("test/index.ejs", {dataUser: JSON.stringify(data)});
+  })
+
+  
+};
 
 module.exports = {
-    getHomepage
-}
+  getHomepage,
+};
