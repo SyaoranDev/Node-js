56e44044498b1438c3a160b53f2336ccac86db2c
Anh Tu Dev <daoanhtu1991@gmail.com>
2023-04-19 22:34:09 +0700

    complete CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -15,18 +15,42 @@ let getDetailPage = async (req, res) => {
 
 let createNewUser = async (req, res) => {
   console.log("check req:", req.body);
-  let {firstName, lastName, email, address} = req.body;
+  let { firstName, lastName, email, address } = req.body;
 
   await pool.execute(
-    'insert into users(firstName, lastName, email, address) values (?,?,?,?)',
+    "insert into users(firstName, lastName, email, address) values (?,?,?,?)",
     [firstName, lastName, email, address]
   );
 
-  return res.redirect('/');
+  return res.redirect("/");
+};
+
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute("Delete from users where id = ?", [userId]);
+  return res.redirect("/");
+};
+
+let editUser = async (req, res) => {
+  let id = req.params.id;
+  let [user] = await pool.execute("Select * from users where id = ?", [id]);
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+
+let updateUser = async (req, res) => {
+  let { firstName, lastName, email, address, id} = req.body;
+  await pool.execute(
+    "Update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?",
+    [firstName, lastName, email, address, id]
+  );
+  return res.redirect("/");
 };
 
 module.exports = {
   getHomepage,
   getDetailPage,
   createNewUser,
+  deleteUser,
+  editUser,
+  updateUser,
 };
