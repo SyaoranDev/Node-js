e79f471ae96c31b122d26f1e4d816dc49e902554
Anh Tu Dev <daoanhtu1991@gmail.com>
2023-04-20 22:19:40 +0700

    Restfull API

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import configViewEngine from './config/viewEngine';
 import initWebRoute from './route/web';
+import initAPIRoute from './route/api';
 // import connection from './config/connectDB' 
 
 require('dotenv').config()
@@ -13,11 +14,12 @@ app.use(express.json());
 
 //setup View Engine
 configViewEngine(app);
+
 //initWebRoute
 initWebRoute(app);
 
-
-
+//initAPIRoute
+initAPIRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
