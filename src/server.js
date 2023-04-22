4ffc267a4de1dfb06e15f5bb0394ebbcf8dbede3
Anh Tu Dev <daoanhtu1991@gmail.com>
2023-04-22 22:43:33 +0700

    MIDDLEWARE

--- src/server.js
+++ src/server.js
@@ -5,10 +5,18 @@ import initAPIRoute from './route/api';
 // import connection from './config/connectDB' 
 
 require('dotenv').config()
+var morgan = require('morgan')
 
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  console.log('check my req')
+  console.log(req.method)
+  next();
+})
+
+app.use(morgan('combined'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,6 +28,11 @@ initWebRoute(app);
 
 //initAPIRoute
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+  return res.render('404.ejs');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
